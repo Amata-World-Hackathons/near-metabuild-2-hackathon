87d7e634fb702f2b7df49a4e2b532675838c16ef
yggie <yggie@users.noreply.github.com>
2022-02-21 13:48:06 +0000

    Automated publish

--- webapp/next.config.js
+++ webapp/next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  assetPrefix: process.env.ASSET_PREFIX || "",
   reactStrictMode: true,
-  assetPrefix: process.env.ASSET_PREFIX,
 };
 
 module.exports = nextConfig;
