7d183afbd0a9c81470920cf5865ed54d3562db75
yggie <yggie@users.noreply.github.com>
2022-02-21 14:34:09 +0000

    Deploying to gh-pages from @ amata-world/near-metabuild-2-hackathon@aecb26a58e9df7e54250f99bab15be717a2c6c2f 🚀

---
+++ _next/static/7M5iI5t4ydH55C0fEPy8-/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-4d7717f9e2ec0fcc.js"],"/_error":["static/chunks/pages/_error-e1af3223e34034d5.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
