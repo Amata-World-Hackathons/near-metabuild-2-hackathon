85385cdc5eedb925219b8c95bd29e11c59b30e03
Bryan Yap <bryan@amata.world>
2022-02-21 13:41:25 +0000

    fix CSS in GH pages

--- webapp/next.config.js
+++ webapp/next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  assetPrefix: process.env.ASSET_PREFIX,
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
